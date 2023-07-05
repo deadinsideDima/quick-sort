f8446fb6b8973109e50c1890d84aaa84c0ec781f
Dmitry Nesterenko <trapwhiteclown@gmail.com>
2023-07-05 13:39:33 +0300

    Quick sort!

--- QuickSort/Sorter.cs
+++ QuickSort/Sorter.cs
@@ -10,8 +10,12 @@ namespace QuickSort
         /// </summary>
         public static void QuickSort(this int[]? array)
         {
-            // TODO #1. Implement the method using a loop statements.
-            throw new NotImplementedException();
+            if (array == null)
+            {
+                throw new ArgumentNullException(nameof(array));
+            }
+
+            RecursiveQuickSort(array);
         }
 
         /// <summary>
@@ -19,8 +23,39 @@ namespace QuickSort
         /// </summary>
         public static void RecursiveQuickSort(this int[]? array)
         {
-            // TODO #2. Implement the method using recursion algorithm.
-            throw new NotImplementedException();
+            if (array == null)
+            {
+                throw new ArgumentNullException(nameof(array));
+            }
+
+            RQuickSort(array, 0, array.Length - 1);
+        }
+
+        public static void RQuickSort(this int[]? array, int start, int end)
+        {
+            if (array == null)
+            {
+                throw new ArgumentNullException(nameof(array));
+            }
+
+            if (start < end)
+            {
+                int index = start;
+                for (int i = start; i <= end; i++)
+                {
+                    if (array[i] <= array[end])
+                    {
+                        int temp = array[index];
+                        array[index] = array[i];
+                        array[i] = temp;
+                        index += 1;
+                    }
+                }
+
+                index -= 1;
+                RQuickSort(array, start, index - 1);
+                RQuickSort(array, index + 1, end);
+            }
         }
     }
 }
